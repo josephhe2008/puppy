ad18db5cdf883050d466a484b9d637ac0fab3c34
Joseph He <joseph.he.2008@gmail.com>
2023-09-02 21:55:25 -0500

    first puppy

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/josephhe2008/gointerview/puppy
+
+go 1.21.0
---
+++ puppy.go
@@ -0,0 +1,10 @@
+package main
+
+
+func Bark() string {
+	return "woof"
+}
+
+func Barks() string {
+	return "woof! woof! woof!"
+}
